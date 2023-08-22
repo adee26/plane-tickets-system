1ff934edc7c5757a699b521deb6f44d8bffd457a
Adelina Apetrei <adelina.apetrei26@gmail.com>
2023-08-22 12:37:58 +0300

    Issue 1 - Create Spring Project

---
+++ src/main/java/com/adelinaportfolio/planeTicketsSystem/PlaneTicketsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.adelinaportfolio.planeTicketsSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneTicketsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaneTicketsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adelinaportfolio/planeTicketsSystem/PlaneTicketsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adelinaportfolio.planeTicketsSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneTicketsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
