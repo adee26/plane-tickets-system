9f01dea6a26a28bc72999703d135c3638351f02e
Adelina Apetrei <adelina.apetrei26@gmail.com>
2023-08-23 01:22:36 +0300

    CPTS-18: test failing test

--- src/test/java/com/adelinaportfolio/planeTicketsSystem/PlaneTicketsSystemApplicationTests.java
+++ src/test/java/com/adelinaportfolio/planeTicketsSystem/PlaneTicketsSystemApplicationTests.java
@@ -3,11 +3,14 @@ package com.adelinaportfolio.planeTicketsSystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class PlaneTicketsSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+		fail();
 	}
 
 }
